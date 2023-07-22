825e186887d8aef977d5c36df6cffb9127e9efbb
luis605 <lusluistomascabetedealmeida@gmail.com>
2023-07-22 18:45:24 +0100

    Game with Raycasts

--- project/game/hi.py
+++ project/game/hi.py
@@ -1,17 +1,17 @@
 velocity = 2
 entity.name = str(entity.position.x)
-
+can_move_forward = True
 
 total_duration = 8.0 
 elapsed_time = 0.0001
 
-target_position  = Vector3(0,0,0)
+target_position  = Vector3(0,1,0)
 def update():
-	global velocity, elapsed_time, total_duration
+	global velocity, elapsed_time, total_duration, can_move_forward
 
 	entity.position = lerp(entity.position, target_position, time.dt)
 
-	if (IsKeyDown(KeyboardKey.KEY_W)):
+	if (IsKeyDown(KeyboardKey.KEY_W) and can_move_forward):
 	    target_position.x -= velocity * time.dt
 	    
 	
@@ -41,9 +41,16 @@ def update():
 	camera.position = Vector3(entity.position.x + 10, entity.position.y + 2, entity.position.z)
 	camera.target = Vector3(entity.position.x, entity.position.y, entity.position.z)
 
-	hit_info = raycast(entity.position, Vector3(0, 1, 0), debug=False, ignore=[entity])
+	hit_info = raycast(target_position, Vector3(1, 0, 0), debug=False, ignore=[entity])
 	if hit_info.hit:
-		hit_info.entity.color.print()
+		if hit_info.distance < 0.1:
+			can_move_forward = False
+			hit_info.entity.color.print()
+		else:
+			can_move_forward = True
+	else:
+		can_move_forward = True
+
 
 
 
