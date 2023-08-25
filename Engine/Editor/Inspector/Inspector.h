ee651d702dc154974c1924f42867c84e15e47129
luis605 <lusluistomascabetedealmeida@gmail.com>
2023-08-25 17:42:38 +0100

    Text Inspector

--- Engine/Editor/Inspector/Inspector.h
+++ Engine/Editor/Inspector/Inspector.h
@@ -1,26 +1,30 @@
 #ifndef INSPECTOR_H
 #define INSPECTOR_H
 
-string selected_entity_name = "";
-Vector3 selected_entity_scale = {1, 1, 1};
-Vector3 selected_entity_position = {0, 0, 0};
-Vector3 selected_entity_relative_position = {0, 0, 0};
-Vector3 selected_entity_rotation = {0, 0, 0};
-Color selected_entity_color = RED;
-ImVec4 selected_entity_colorImGui = { 0, 0, 0, 0 };
-string selected_entity_script_path;
+string selected_entity_name                    = "";
+Vector3 selected_entity_scale                  = {1, 1, 1};
+Vector3 selected_entity_position               = {0, 0, 0};
+Vector3 selected_entity_relative_position      = {0, 0, 0};
+Vector3 selected_entity_rotation               = {0, 0, 0};
+Color selected_entity_color                    = RED;
+ImVec4 selected_entity_colorImGui              = { 0, 0, 0, 0 };
+string selected_entity_script_path             = "";
 Texture2D entity_texture;
-int selected_entity_script_path_index;
-int selected_entity_model_path_index;
-bool do_physics = false;
+int selected_entity_script_path_index          = 0;
+int selected_entity_model_path_index           = 0;
+bool do_physics                                = false;
 
-bool show_texture = false;
-bool show_normal_texture = false;
+bool show_texture                              = false;
+bool show_normal_texture                       = false;
 
 
-bool AttenuationActiveInputMode = false;
-bool IntensityActiveInputMode = false;
-bool SpecularStrenghtActiveInputMode = false;
-bool CutoffActiveInputMode = false;
+bool AttenuationActiveInputMode                = false;
+bool IntensityActiveInputMode                  = false;
+bool SpecularStrenghtActiveInputMode           = false;
+bool CutoffActiveInputMode                     = false;
 
+bool FontSizeActiveInputMode                   = false;
+bool TextSpacingActiveInputMode                = false;
+bool TextBackgroundRoundinessActiveInputMode   = false;
+bool TextPaddingActiveInputMode                = false;
 #endif // INSPECTOR_H
