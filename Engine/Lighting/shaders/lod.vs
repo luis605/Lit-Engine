adb706ae0a860cc2f3b8eae98849f2952d2001f4
luis605 <lusluistomascabetedealmeida@gmail.com>
2023-09-27 21:16:50 +0100

    Fix Segfault

--- Engine/Lighting/shaders/lod.vs
+++ Engine/Lighting/shaders/lod.vs
@@ -1,4 +1,4 @@
-#version 330
+#version 330 core
 
 // Input vertex attributes
 in vec3 vertexPosition;
@@ -16,18 +16,40 @@ out vec2 fragTexCoord;
 out vec4 fragColor;
 out vec3 fragNormal;
 
-// NOTE: Add here your custom variables
+// Custom variables
+uniform float distanceThreshold = 3.0; // Distance at which to switch LODs
+uniform vec3 viewPos;
+uniform float popDistance = 1.0; // Distance by which to pop vertices
 
 void main()
 {
-    // Send vertex attributes to fragment shader
-    fragPosition = vec3(matModel*vec4(vertexPosition, 1.0f));
-    fragTexCoord = vertexTexCoord;
-    fragColor = vertexColor;
-    
-    mat3 normalMatrix = transpose(inverse(mat3(matModel)));
-    fragNormal = normalize(normalMatrix*vertexNormal);
+    // Calculate the distance from the camera
+    float distanceToCamera = length(vertexPosition - viewPos);
 
-    // Calculate final vertex position
-    gl_Position = mvp*vec4(vertexPosition, 1.0);
+    // Calculate LOD factor based on distance
+    float lodFactor = distanceToCamera / distanceThreshold;
+
+    // If LOD factor is below a threshold, pop the vertices
+    if (lodFactor <= 10.0) {
+        // Calculate the vector from the vertex to the camera
+        vec3 toCamera = viewPos - vertexPosition;
+        
+        // Normalize and scale the vector to achieve the popping effect
+        vec3 poppedPosition = vertexPosition + normalize(toCamera) * popDistance;
+        vec4 poppedMVP = mvp * vec4(poppedPosition, 1.0);
+
+        // Send popped attributes to the fragment shader
+        fragPosition = poppedPosition;
+        fragTexCoord = vertexTexCoord;
+        fragColor = vertexColor;
+
+        mat3 normalMatrix = transpose(inverse(mat3(matModel)));
+        fragNormal = normalize(normalMatrix * vertexNormal);
+
+        // Calculate final vertex position
+        gl_Position = poppedMVP;
+    }
+    else {
+        return;
+    }
 }
--- sandbox.cpp
+++ sandbox.cpp
@@ -1,62 +1,70 @@
 #include "raylib.h"
 
-int main(void)
+#include "raymath.h"
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+int main()
 {
     SetTraceLogLevel(LOG_WARNING);
+
+    // Initialization
     const int screenWidth = 800;
-    const int screenHeight = 600;
+    const int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "CSM");
 
-    InitWindow(screenWidth, screenHeight, "Raylib Texture Tiling");
-
-    // Load a texture
-    Texture2D texture = LoadTexture("icon.png");
-
-    // Create a cube mesh
-    Mesh cube = GenMeshCube(1.0f, 1.0f, 1.0f);
-
-    // Load the texture onto the GPU
-    Model model = LoadModelFromMesh(cube);
-    model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
-
-    // Set the tiling of the texture
-    float tiling[2] = {5.0f, 5.0f};
-    Shader shader = LoadShader(0, "custom.shader"); // Create a custom shader in a .glsl file
-    SetShaderValue(shader, GetShaderLocation(shader, "tiling"), tiling, SHADER_UNIFORM_VEC2);
-    model.materials[0].shader = shader;
-
-    // Camera setup
-    Camera camera = { 0 };
-    camera.position = (Vector3){ 3.0f, 3.0f, 3.0f };
+    // Setup camera
+    Camera3D camera = { 0 };
+    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f };
     camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
     camera.fovy = 45.0f;
     camera.projection = CAMERA_PERSPECTIVE;
+    
+    SetTargetFPS(60);
+
+    Shader shader = LoadShader("Engine/Lighting/shaders/lod.vs", "Engine/Lighting/shaders/lod.fs");
+    Shader shader2 = LoadShader(0, "Engine/Lighting/shaders/lod.fs");
+
+    Model plane = LoadModelFromMesh(GenMeshPlane(10.0f, 10.0f, 1, 1));
+    Model cube = LoadModelFromMesh(GenMeshCylinder(1,3,100));
+    Model cube_simple = LoadModelFromMesh(GenMeshCylinder(1,3,100));
+
+    plane.materials[0].shader = shader;
+    cube.materials[0].shader = shader;
+    cube_simple.materials[0].shader = shader2;
+
+    DisableCursor();
 
     // Main game loop
     while (!WindowShouldClose())
     {
+        
+        SetShaderValue(shader, GetShaderLocation(shader, "viewPos"), &camera.position, SHADER_UNIFORM_VEC3);
+        UpdateCamera(&camera, CAMERA_FREE);
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        UpdateCamera(&camera, CAMERA_FREE);
 
-        // Draw the model
+
         {
+            ClearBackground(GRAY);
             BeginMode3D(camera);
-            BeginShaderMode(shader);
-
-            DrawModel(model, (Vector3){ 0.0f, 0.0f, 0.0f }, 5.0f, WHITE);
-
-            EndShaderMode();
+            {
+                BeginShaderMode(shader);
+                    DrawModel(plane, Vector3{0, 0, 0}, 4.0f, WHITE);
+                    DrawModel(cube, Vector3{0, 3, 10}, 1.0f, RED);
+                    DrawModel(cube_simple, Vector3{0, 3, 0}, 1.0f, WHITE);
+                EndShaderMode();
             EndMode3D();
+            }EndTextureMode();
         }
         
-        DrawText("Use mouse to rotate the camera", 10, 10, 20, DARKGRAY);
 
         EndDrawing();
     }
 
-    UnloadTexture(texture);
-    UnloadModel(model);
     CloseWindow();
 
     return 0;
