02aeda6789e1f06fe4197ecdba06ecdfb5fc6a47
luis605 <lusluistomascabetedealmeida@gmail.com>
2023-10-08 20:31:09 +0100

    Faster Approach

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,109 +1,206 @@
+#include "include/raylib.h"
+#include "include/raymath.h"
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <cfloat>
+#include <limits>
 #include <cmath>
-#include "include/raylib.h"
+#include <unordered_map>
+#include <omp.h>
+#include <queue>
+#include <algorithm>
+#include <execution>
+#include <unordered_map>
 
-// Define a custom hash function for Vector3
-struct Vector3Hash {
-    std::size_t operator()(const Vector3& v) const {
-        std::size_t h1 = std::hash<float>{}(v.x);
-        std::size_t h2 = std::hash<float>{}(v.y);
-        std::size_t h3 = std::hash<float>{}(v.z);
-        return h1 ^ (h2 << 1) ^ (h3 << 2);
-    }
-};
 
-// Define a custom equality comparison function for Vector3
-struct Vector3Equal {
-    bool operator()(const Vector3& v1, const Vector3& v2) const {
-        return v1.x == v2.x && v1.y == v2.y && v1.z == v2.z;
-    }
-};
-
-std::unordered_map<Vector3, Vector3, Vector3Hash, Vector3Equal> AssignGridIDs(const std::vector<Vector3>& vertices, float gridSize) {
-    std::unordered_map<Vector3, Vector3, Vector3Hash, Vector3Equal> vertexGridID;
-    
-    // Calculate the grid position for each vertex
-    for (int i = 0; i < vertices.size(); ++i) {
-        const Vector3& vertex = vertices[i];
-        
-        int gridX = static_cast<int>(std::floor(vertex.x / gridSize));
-        int gridY = static_cast<int>(std::floor(vertex.y / gridSize));
-        int gridZ = static_cast<int>(std::floor(vertex.z / gridSize));
-        
-        // Create a Vector3 grid position
-        Vector3 gridPosition = { gridX * gridSize, gridY * gridSize, gridZ * gridSize };
-        
-        // Store the vertex position and its corresponding grid position in the unordered_map
-        vertexGridID[vertex] = gridPosition;
-    }
-    
-    return vertexGridID;
+// Function to calculate the midpoint between two vertices
+Vector3 CalculateMidpoint(const Vector3& vertex1, const Vector3& vertex2) {
+    return Vector3{ (vertex1.x + vertex2.x) / 2.0f, (vertex1.y + vertex2.y) / 2.0f, (vertex1.z + vertex2.z) / 2.0f };
 }
 
-std::vector<Vector3> GetNearbyVertices(const std::unordered_map<Vector3, Vector3, Vector3Hash, Vector3Equal>& vertexGridID, const Vector3& targetVertex, float gridSize) {
-    std::vector<Vector3> nearbyVertices;
+inline float Vector3DistanceSquared(const Vector3& a, const Vector3& b) {
+    float dx = a.x - b.x;
+    float dy = a.y - b.y;
+    float dz = a.z - b.z;
+    return dx * dx + dy * dy + dz * dz;
+}
 
-    // Calculate the grid position of the target vertex
-    int gridX = static_cast<int>((targetVertex.x / gridSize));
-    int gridY = static_cast<int>((targetVertex.y / gridSize));
-    int gridZ = static_cast<int>((targetVertex.z / gridSize));
+// Define the spatial hash cell size (adjust as needed)
+const float cellSize = 0.1f; // You can adjust this value
 
-    // Iterate through nearby grid positions (e.g., a 3x3x3 cube of grids)
-    for (int xOffset = -1; xOffset <= 1; ++xOffset) {
-        for (int yOffset = -1; yOffset <= 1; ++yOffset) {
-            for (int zOffset = -1; zOffset <= 1; ++zOffset) {
-                // Calculate the neighboring grid position
-                int nearbyGridX = gridX + xOffset;
-                int nearbyGridY = gridY + yOffset;
-                int nearbyGridZ = gridZ + zOffset;
+std::vector<Vector3> ContractVertices(const Mesh& mesh, float maxDistance) {
+    if (mesh.vertices == nullptr || mesh.vertexCount == 0) {
+        std::cout << "Mesh has no vertices or is not initialized." << std::endl;
+        return std::vector<Vector3>();
+    }
 
-                // Create a Vector3 representing the nearby grid position
-                Vector3 nearbyGridPosition{
-                    nearbyGridX * gridSize,
-                    nearbyGridY * gridSize,
-                    nearbyGridZ * gridSize
-                };
+    const float maxDistanceSquared = maxDistance * maxDistance;
 
-                // Check if the nearby grid position exists in the map
-                auto it = vertexGridID.find(nearbyGridPosition);
-                if (it != vertexGridID.end()) {
-                    // If it exists, add the associated vertex to the result
-                    nearbyVertices.push_back(it->second);
+    std::vector<Vector3> contractedVertices(mesh.vertexCount, Vector3{0.0f, 0.0f, 0.0f});
+
+    // Sort vertices by x-coordinate for better memory access patterns
+    std::vector<int> sortedIndices(mesh.vertexCount);
+    #pragma omp parallel for
+    for (int i = 0; i < mesh.vertexCount; i++) {
+        sortedIndices[i] = i;
+    }
+    std::sort(sortedIndices.begin(), sortedIndices.end(), [&](int a, int b) {
+        return mesh.vertices[a * 3] < mesh.vertices[b * 3];
+    });
+
+    #pragma omp parallel for
+    for (int i = 0; i < mesh.vertexCount; i++) {
+        int idx = sortedIndices[i];
+        float xi = mesh.vertices[idx * 3];
+        float yi = mesh.vertices[idx * 3 + 1];
+        float zi = mesh.vertices[idx * 3 + 2];
+        Vector3 vertex_position = { xi, yi, zi };
+
+        int closestVertexIndex = -1;
+        float closestDistance = maxDistanceSquared;
+
+        // Only search within a limited neighborhood
+        int searchStart = std::max(0, i - 128); // Adjust the neighborhood size as needed
+
+        for (int j = searchStart; j < i; j++) {
+            int jdx = sortedIndices[j];
+            float distSq = Vector3DistanceSquared(vertex_position, contractedVertices[jdx]);
+            if (distSq <= closestDistance) {
+                closestVertexIndex = jdx;
+                closestDistance = distSq;
+
+                // Early exit if a very close vertex is found
+                if (distSq < maxDistanceSquared * 0.25f) {
+                    break;
                 }
             }
         }
+
+        contractedVertices[idx] = (closestVertexIndex != -1) ? contractedVertices[closestVertexIndex] : vertex_position;
     }
 
-    return nearbyVertices;
+    sortedIndices.clear();
+
+    return contractedVertices;
 }
 
+
+
+
+
+// Function to generate a simplified LOD mesh
+Mesh GenerateLODMesh(const std::vector<Vector3>& uniqueVertices, Mesh& sourceMesh) {
+    Mesh lodMesh = { 0 };
+
+    if (!uniqueVertices.empty()) {
+        int vertexCount = uniqueVertices.size();
+        int triangleCount = vertexCount / 3;
+        int indexCount = triangleCount * 3;
+
+        // Allocate memory for the new mesh
+        lodMesh.vertexCount = vertexCount;
+        lodMesh.triangleCount = triangleCount;
+        lodMesh.vertices = (float*)malloc(sizeof(float) * 3 * vertexCount);
+        lodMesh.indices = (unsigned short*)malloc(sizeof(unsigned short) * indexCount);
+//        lodMesh.indices = sourceMesh.indices;
+
+        // Copy unique vertices to the new mesh's vertex array
+        for (int i = 0; i < vertexCount; i++) {
+            lodMesh.vertices[i * 3] = uniqueVertices[i].x;
+            lodMesh.vertices[i * 3 + 1] = uniqueVertices[i].y;
+            lodMesh.vertices[i * 3 + 2] = uniqueVertices[i].z;
+        }
+
+        // Generate new indices for non-indexed mesh
+        if (sourceMesh.indices)
+        {
+            for (int i = 0; i < triangleCount; i++) {
+                lodMesh.indices[i * 3] = sourceMesh.indices[i * 3];
+                lodMesh.indices[i * 3 + 1] = sourceMesh.indices[i * 3 + 1];
+                lodMesh.indices[i * 3 + 2] = sourceMesh.indices[i * 3 + 2];
+            }
+        }
+        else
+            lodMesh.indices = sourceMesh.indices;
+    }
+    UploadMesh(&lodMesh, false);
+    return lodMesh;
+}
+
+
+
+
 int main() {
-    std::vector<Vector3> vertices = {
-        {0.0f, 0.0f, 0.0f},
-        {2.0f, 0.0f, 0.0f},
-        {0.0f, 2.0f, 0.0f},
-        {2.0f, 5.0f, 0.0f},
-        {0.0f, 0.0f, 2.0f},
-        {2.0f, 0.0f, 4.0f},
-        {0.0f, 2.0f, 3.0f},
-        {2.0f, 2.0f, 2.0f}
-    };
+    SetTraceLogLevel(LOG_WARNING);
 
-    float gridSize = 2.0f; // Adjust the grid size as needed
-    
-    std::unordered_map<Vector3, Vector3, Vector3Hash, Vector3Equal> vertexGridID = AssignGridIDs(vertices, gridSize);
+    const int screenWidth = 800;
+    const int screenHeight = 600;
 
-    // Get nearby vertices of a specific target vertex (e.g., vertex 0)
-    Vector3 targetVertex = {0.0f, 0.0f, 0.0f};
-    std::vector<Vector3> nearbyVertices = GetNearbyVertices(vertexGridID, targetVertex, gridSize);
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(screenWidth, screenHeight, "LOD Example");
 
-    // Output the nearby vertices
-    std::cout << "Nearby vertices of target vertex (" << targetVertex.x << ", " << targetVertex.y << ", " << targetVertex.z << "):" << std::endl;
-    for (const Vector3& nearbyVertex : nearbyVertices) {
-        std::cout << "Vertex (" << nearbyVertex.x << ", " << nearbyVertex.y << ", " << nearbyVertex.z << ")" << std::endl;
+    std::cout << "Starting Program..." << std::endl;
+
+    Shader shader = LoadShader(0, "Engine/Lighting/shaders/lod.fs");
+
+    // Starting LOD level
+    Mesh sourceMesh = GenMeshSphere(1, 99, 99);//LoadModel("a.obj").meshes[0];
+
+    std::cout << "loaded" << std::endl;
+
+    float lodFactor = 0;
+    // Get unique vertices and generate LOD mesh
+    Model lodModel = LoadModelFromMesh(GenMeshCube(1,1,1));
+    lodModel.materials[0].shader = shader;
+
+    Camera3D camera = { 0 };
+    camera.position = (Vector3){ 0.0f, 0.0f, 5.0f };
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    SetTargetFPS(60);
+
+    // Use the generated LOD mesh as needed
+    while (!WindowShouldClose()) {
+
+        if (IsKeyPressed(KEY_O))
+        {
+            lodFactor += 0.1f;
+        }
+        else if (IsKeyPressed(KEY_P))
+        {
+            lodFactor -= 0.1f;
+            std::cout << "DECREASING\n";
+        }
+
+        std::vector<Vector3> uniqueVertices = ContractVertices(sourceMesh, lodFactor);
+        lodModel.meshes[0] = GenerateLODMesh(uniqueVertices, sourceMesh);
+
+
+
+        BeginDrawing();
+        ClearBackground(WHITE);
+        UpdateCamera(&camera, CAMERA_FREE);
+        SetShaderValue(shader, GetShaderLocation(shader, "viewPos"), &camera.position, SHADER_UNIFORM_VEC3);
+
+        BeginMode3D(camera);
+        BeginShaderMode(shader);
+
+        DrawModel(lodModel, Vector3Zero(), 1.0f, WHITE);
+
+        
+        EndShaderMode();
+        EndMode3D();
+
+        DrawFPS(10,10);
+        EndDrawing();
     }
 
+    UnloadModel(lodModel);
+    UnloadMesh(sourceMesh);
+    UnloadShader(shader);
+    CloseWindow();
     return 0;
 }
