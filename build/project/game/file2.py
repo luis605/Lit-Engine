977d29cffa428f62a678c3525a89c0c3f40492d7
luis605 <lusluistomascabetedealmeida@gmail.com>
2023-10-14 19:19:58 +0100

    Updated folders so Lit Engine runs on windows

---
+++ build/project/game/file2.py
@@ -0,0 +1,25 @@
+#import random
+entities_number = 1500
+for i in range(entities_number):
+	if i == entities_number:
+		continue
+	print(i)
+	Entity()
+#for i in range(20000):
+#	entity1 = Entity()
+#	x = random.uniform(-10, 10)
+#	z = random.uniform(-10, 10)
+#	entity1.position.x = x
+#	entity1.position.z = z
+
+
+
+
+
+
+
+
+
+
+
+
