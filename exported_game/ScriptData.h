1ea60d996064d74a6987ecc9575137872811f11f
luis605 <lusluistomascabetedealmeida@gmail.com>
2023-10-20 19:46:05 +0100

    Skybox in RunGame.cpp

--- exported_game/ScriptData.h
+++ exported_game/ScriptData.h
@@ -1,2 +1,29 @@
 std::map<std::string, const char*> scriptMap = {
+    {"file10", R"(
+
+
+def update():
+	normalFovy = 60.0
+	runningFovy = 160.0
+	
+	if (IsKeyDown(KeyboardKey.KEY_W)):
+		camera.fovy = lerp(camera.fovy, runningFovy, time.dt)
+	else:
+		camera.fovy = lerp(camera.fovy, normalFovy, time.dt)
+
+
+	camera.pos = Vector3(entity.position.x, entity.position.y, entity.position.z)
+	camera.pos.y = -10
+	camera.look_at = Vector3(entity.position.x + 0.1, entity.position.y, entity.position.x + 0)
+
+
+
+
+
+
+
+
+
+
+)"},
 };
--- project/game/file1.py
+++ project/game/file1.py
@@ -11,10 +11,14 @@ def update():
 
 
 	camera.pos = Vector3(entity.position.x, entity.position.y, entity.position.z)
-	camera.pos.y = -10
+	camera.pos.x = -5
+	camera.pos.z = 10
+	camera.pos.y = 2
 	camera.look_at = Vector3(entity.position.x + 0.1, entity.position.y, entity.position.x + 0)
 
-	camera.up = Vector3(0, 1, 0)
+
+
+
 
 
 
