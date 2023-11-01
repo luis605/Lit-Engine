4f285d629d916c24503c38649d4324597efe6c56
luis605 <lusluistomascabetedealmeida@gmail.com>
2023-11-01 20:35:10 +0000

    Removed folder

--- build - Cópia/CMakeFiles/bdb.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/bdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/bdb.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build - Cópia/CMakeFiles/lit_engine.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for lit_engine.
-# This may be replaced when dependencies are built.
