15c9766ba299fed5b8581d6d33acbc9c83112848
luis605 <lusluistomascabetedealmeida@gmail.com>
2023-11-01 20:35:10 +0000

    Removed folder

--- build - Cópia/CMakeFiles/build_and_run.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/build_and_run"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/build_and_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
