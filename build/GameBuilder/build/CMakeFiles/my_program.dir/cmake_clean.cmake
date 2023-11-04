b6d4b76c88246406ad0d12c1ebfa6f2cf47f9ee6
luis605 <lusluistomascabetedealmeida@gmail.com>
2023-11-04 18:51:41 +0000

    Game Exporter working on windows

---
+++ build/GameBuilder/build/CMakeFiles/my_program.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_program.dir/gameSystem.cpp.obj"
+  "CMakeFiles/my_program.dir/gameSystem.cpp.obj.d"
+  "libmy_program.dll.a"
+  "my_program.exe"
+  "my_program.exe.manifest"
+  "my_program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
