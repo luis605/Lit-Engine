1d25bc6a84119aa61532e055fcd554930592cc2d
luis605 <lusluistomascabetedealmeida@gmail.com>
2023-12-24 19:36:33 +0000

    Created windows build

--- build/GameBuilder/build/cmake_install.cmake
+++ build/GameBuilder/build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/USER/Desktop/_/Lit-Engine
+# Install script for directory: C:/Users/USER/Desktop/_/Lit-Engine/build/GameBuilder
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/LitEngine")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/MyProgram")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/USER/Desktop/_/Lit-Engine/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/USER/Desktop/_/Lit-Engine/build/GameBuilder/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
