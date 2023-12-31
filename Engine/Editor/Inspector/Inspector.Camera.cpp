c8b1a58ff5719c1d01d4e3961a7629e322153979
luis605 <lusluistomascabetedealmeida@gmail.com>
2023-12-31 20:14:35 +0000

    Styled Camera Inspector

--- Engine/Editor/Inspector/Inspector.Camera.cpp
+++ Engine/Editor/Inspector/Inspector.Camera.cpp
@@ -2,39 +2,40 @@
 
 void CameraInspector()
 {
-
     ImVec2 window_size = ImGui::GetWindowSize();
+    const float inputWidth = 200.0f;
 
     ImGui::Text("Inspecting Camera");
-    ImGui::SameLine();
+    ImGui::Dummy(ImVec2(0.0f, 10.0f));
 
     ImGui::BeginChild("MainContent", window_size);
 
     if (ImGui::CollapsingHeader("Camera Properties"))
     {
-        {
-            bool isOrthographic = (scene_camera.projection == 1) ? true : false;
-            
-            if (ImGui::BeginCombo("Projection", isOrthographic ? "Orthographic" : "Perspective")) {
-                if (ImGui::Selectable("Orthographic", isOrthographic)) {
-                    isOrthographic = true;
-                    scene_camera.projection = 1;
-                }
-                if (ImGui::Selectable("Perspective", !isOrthographic)) {
-                    isOrthographic = false;
-                    scene_camera.projection = 0;
-                }
-                ImGui::EndCombo();
+        ImGui::Indent(10.0f);
+
+        bool isOrthographic = (scene_camera.projection == 1) ? true : false;
+
+        ImGui::Text("Camera Projection: ");
+        ImGui::SameLine(inputWidth);
+        if (ImGui::BeginCombo("Projection", isOrthographic ? "Orthographic" : "Perspective")) {
+            if (ImGui::Selectable("Orthographic", isOrthographic)) {
+                isOrthographic = true;
+                scene_camera.projection = 1;
             }
+            if (ImGui::Selectable("Perspective", !isOrthographic)) {
+                isOrthographic = false;
+                scene_camera.projection = 0;
+            }
+            ImGui::EndCombo();
         }
 
-        {
-            ImGui::Text("Fovy: ");
-            ImGui::SameLine();
-            ImGui::SliderFloat("##FovySlider", &scene_camera.fovy, 1.0f, 180.0f, ".1f");
-        }
 
+        ImGui::Text("Fovy: ");
+        ImGui::SameLine(inputWidth);
+        ImGui::SliderFloat("##FovySlider", &scene_camera.fovy, 1.0f, 180.0f, ".1f");
 
+        ImGui::Unindent(10.0f);
     }
 
     ImGui::EndChild();
