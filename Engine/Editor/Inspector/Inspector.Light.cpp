06f144baea49c4aec7e7dcee1e71bb56f5c731ea
luis605 <lusluistomascabetedealmeida@gmail.com>
2024-01-01 14:15:07 +0000

    White Space

--- Engine/Editor/Inspector/Inspector.Light.cpp
+++ Engine/Editor/Inspector/Inspector.Light.cpp
@@ -8,38 +8,7 @@ void LightInspector()
         selected_light = std::get<Light*>(object_in_inspector);
     }
 
-    ImVec4 light_colorImGui = ImVec4(
-        selected_light->color.r,
-        selected_light->color.g,
-        selected_light->color.b,
-        selected_light->color.a
-    );
-
-    ImGui::Text("Color: ");
-    ImGui::ColorEdit4("##Change_Light_Color", (float*)&light_colorImGui, ImGuiColorEditFlags_NoInputs);
-    glm::vec4 light_color = {
-        (float)(light_colorImGui.x),
-        (float)(light_colorImGui.y),
-        (float)(light_colorImGui.z),
-        (float)(light_colorImGui.w)
-    };
-
-    selected_light->color = light_color;
-
-    static const char* lights_types[] = { "Directional Light", "Point Light", "Spot Light" };
-    static int currentItem = selected_light->type;
-
-    if (ImGui::BeginCombo("Light Types", lights_types[currentItem])) {
-        for (int index = 0; index < IM_ARRAYSIZE(lights_types); index++) {
-            const bool isSelected = (currentItem == index);
-            if (ImGui::Selectable(lights_types[index], isSelected))
-                currentItem = index;
-                selected_light->type = currentItem;
-            if (isSelected)
-                ImGui::SetItemDefaultFocus();
-        }
-        ImGui::EndCombo();
-    }
+    ImGui::Dummy(ImVec2(0.0f, 15.0f));
 
 
     if (ImGui::CollapsingHeader("Light Direction")) {
@@ -61,6 +30,46 @@ void LightInspector()
 
     if (ImGui::CollapsingHeader("Light Properties")) {
         ImGui::Indent(30.0f);
+
+
+
+        ImVec4 light_colorImGui = ImVec4(
+            selected_light->color.r,
+            selected_light->color.g,
+            selected_light->color.b,
+            selected_light->color.a
+        );
+
+        ImGui::Text("Color: ");
+        ImGui::ColorEdit4("##Change_Light_Color", (float*)&light_colorImGui, ImGuiColorEditFlags_NoInputs);
+        glm::vec4 light_color = {
+            (float)(light_colorImGui.x),
+            (float)(light_colorImGui.y),
+            (float)(light_colorImGui.z),
+            (float)(light_colorImGui.w)
+        };
+
+        selected_light->color = light_color;
+
+        static const char* lights_types[] = { "Directional Light", "Point Light", "Spot Light" };
+        static int currentItem = selected_light->type;
+
+        if (ImGui::BeginCombo("Light Types", lights_types[currentItem])) {
+            for (int index = 0; index < IM_ARRAYSIZE(lights_types); index++) {
+                const bool isSelected = (currentItem == index);
+                if (ImGui::Selectable(lights_types[index], isSelected))
+                    currentItem = index;
+                    selected_light->type = currentItem;
+                if (isSelected)
+                    ImGui::SetItemDefaultFocus();
+            }
+            ImGui::EndCombo();
+        }
+
+
+        ImGui::Dummy(ImVec2(0.0f, 15.0f));
+
+
 
         if (!AttenuationActiveInputMode) {
             ImGui::SliderFloat("Attenuation", &selected_light->attenuation, 0.0f, 1.0f);
