0f076d9b9d111b8f193ae2dc68c2a1970b2f7286
luis605 <lusluistomascabetedealmeida@gmail.com>
2024-01-07 15:44:46 +0000

    Removed old code

--- Engine/Core/Core.cpp
+++ Engine/Core/Core.cpp
@@ -20,18 +20,6 @@ void segfault_sigaction(int signal, siginfo_t *si, void *arg)
 
 void Startup()
 {
-/*
-    // Core - Safety
-    struct sigaction sa;
-
-    memset(&sa, 0, sizeof(struct sigaction));
-    sigemptyset(&sa.sa_mask);
-    sa.sa_sigaction = segfault_sigaction;
-    sa.sa_flags   = SA_SIGINFO;
-
-    sigaction(SIGSEGV, &sa, NULL);
-*/
-
     // Raylib
     SetTraceLogLevel(LOG_WARNING);
 
