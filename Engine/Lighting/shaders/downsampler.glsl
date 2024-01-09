99353eb1c991c7d7a4922b190aa4e1c3347eecc1
luis605 <lusluistomascabetedealmeida@gmail.com>
2024-01-09 18:03:26 +0000

    Improved bloom quality

--- Engine/Lighting/shaders/downsampler.glsl
+++ Engine/Lighting/shaders/downsampler.glsl
@@ -6,21 +6,20 @@ uniform sampler2D srcTexture;
 uniform float bloomBrightness;
 
 void main() {
-    float weight = 1.0 / (2.0 * 5.0 + 1.0); // Adjust the weight for a more intense bloom effect
     vec2 texelSize = 1.0 / textureSize(srcTexture, 0);
-    vec3 result = texture(srcTexture, fragTexCoord).rgb * (1.0 - 9.0 * weight);
+    vec3 result = texture(srcTexture, fragTexCoord).rgb;
 
     // Horizontal blur pass
     for (int x = -5; x <= 5; x++) {
         vec2 offset = vec2(x, 0) * texelSize * 5.0;
-        result += texture(srcTexture, fragTexCoord + offset).rgb * weight;
+        result += texture(srcTexture, fragTexCoord + offset).rgb / 11.0;
     }
 
     // Vertical blur pass
     for (int y = -5; y <= 5; y++) {
         vec2 offset = vec2(0, y) * texelSize * 5.0;
-        result += texture(srcTexture, fragTexCoord + offset).rgb * weight;
+        result += texture(srcTexture, fragTexCoord + offset).rgb / 11.0;
     }
 
-    FragColor = vec4(result, 1.0);
+    FragColor = vec4(result * bloomBrightness, 1.0);
 }
