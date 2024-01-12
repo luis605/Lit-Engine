b960142f3d3f6cf342b5799b3cfe7d5bfaddf91f
luis605 <lusluistomascabetedealmeida@gmail.com>
2024-01-12 19:22:38 +0000

    Improved bloom quality

--- Engine/Lighting/shaders/downsampler.glsl
+++ Engine/Lighting/shaders/downsampler.glsl
@@ -1,14 +1,14 @@
 #version 330 core
 
-const float stepSize = 3.0;
+const float stepSize = 4.0;
 
 in vec2 fragTexCoord;
 out vec4 FragColor;
 uniform sampler2D srcTexture;
 uniform float bloomBrightness;
 
-const float weightConstant = 0.0006;
-float weight = weightConstant * (1.0 + bloomBrightness) * 8;
+const float weightConstant = 0.00025;
+float weight = weightConstant * (1.0 + bloomBrightness) * 10;
 float attenuation = 1.0 - 9.0 * weight;
 vec2 texelSize = vec2(1.0) / textureSize(srcTexture, 0);
 
@@ -20,19 +20,13 @@ void main() {
 
     vec2 offsetStep = texelSize * stepSize;
 
-    for (int i = -samples; i <= samples; i += 3) {
-        for (int j = -samples; j <= samples; j += 3) {
+    for (int i = -samples; i <= samples; ++i) {
+        for (int j = -samples; j <= samples; ++j) {
             vec2 offset = vec2(i, j) * offsetStep;
             vec2 texCoordOffset = fragTexCoord + offset;
 
-            vec3 tex1 = texture(srcTexture, texCoordOffset).rgb;
-            result += tex1 * weight;
-
-            offset += offsetStep; // Reuse offsetStep
-            texCoordOffset = fragTexCoord + offset;
-
-            vec3 tex2 = texture(srcTexture, texCoordOffset).rgb;
-            result += tex2 * weight;
+            vec3 texSample = texture(srcTexture, texCoordOffset).rgb;
+            result += texSample * weight;
         }
     }
 
