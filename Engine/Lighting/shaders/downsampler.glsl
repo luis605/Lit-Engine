3bd7180ee19ede7c2775c3ad070fe1fdeea0bdf1
luis605 <lusluistomascabetedealmeida@gmail.com>
2024-01-12 13:06:24 +0000

    Optimized bloom to reach 900 fps locally

--- Engine/Lighting/shaders/downsampler.glsl
+++ Engine/Lighting/shaders/downsampler.glsl
@@ -1,26 +1,37 @@
 #version 330 core
 
-const float weightConstant = 1.0 / 2000.0;
-const int samples = 9;
-const float stepSize = 5.0;
+const int samples = 6;
+const float stepSize = 3.0;
 
 in vec2 fragTexCoord;
 out vec4 FragColor;
 uniform sampler2D srcTexture;
 uniform float bloomBrightness;
 
-void main() {
-    float weight = weightConstant * (1.0 + bloomBrightness) * 2;
-    vec2 texelSize = 1.0 / textureSize(srcTexture, 0);
-    vec3 result = texture(srcTexture, fragTexCoord).rgb * (1.0 - 9.0 * weight);
+const float weightConstant = 0.0006;
+float weight = weightConstant * (1.0 + bloomBrightness) * 8;
+float attenuation = 1.0 - 9.0 * weight;
+vec2 texelSize = vec2(1.0) / textureSize(srcTexture, 0);
 
-    // Unroll the loops
-    for (int i = -samples; i <= samples; i += 2) {
-        for (int j = -samples; j <= samples; j += 2) {
-            vec2 offset1 = vec2(i, j) * texelSize * stepSize;
-            vec2 offset2 = vec2(i + 1, j + 1) * texelSize * stepSize;
-            result += (texture(srcTexture, fragTexCoord + offset1).rgb +
-                       texture(srcTexture, fragTexCoord + offset2).rgb) * weight * 2.0;
+void main() {
+    vec3 result = texture(srcTexture, fragTexCoord).rgb;
+    result *= attenuation;
+
+    vec2 offsetStep = texelSize * stepSize;
+
+    for (int i = -samples; i <= samples; i += 3) {
+        for (int j = -samples; j <= samples; j += 3) {
+            vec2 offset = vec2(i, j) * offsetStep;
+            vec2 texCoordOffset = fragTexCoord + offset;
+
+            vec3 tex1 = texture(srcTexture, texCoordOffset).rgb;
+            result += tex1 * weight;
+
+            offset += offsetStep; // Reuse offsetStep
+            texCoordOffset = fragTexCoord + offset;
+
+            vec3 tex2 = texture(srcTexture, texCoordOffset).rgb;
+            result += tex2 * weight;
         }
     }
 
