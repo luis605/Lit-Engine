7ca4a9b6085719f6659b85363ac99ec6c6d0cb85
luis605 <lusluistomascabetedealmeida@gmail.com>
2024-01-13 13:42:49 +0000

    reviewed comments

--- Engine/Editor/AssetsExplorer/file_manipulation.h
+++ Engine/Editor/AssetsExplorer/file_manipulation.h
@@ -7,7 +7,6 @@ std::string generateNumberedFileName(const fs::path& directoryPath, const std::s
     fs::path filePath;
 
     do {
-        // Construct the file name with the number and extension
         std::string fileName = "file" + std::to_string(fileNumber) + "." + extension;
         filePath = directoryPath / fileName;
         fileNumber++;
@@ -17,10 +16,8 @@ std::string generateNumberedFileName(const fs::path& directoryPath, const std::s
 }
 
 bool createNumberedFile(const fs::path& directoryPath, const std::string& extension) {
-    // Generate the numbered file name
     std::string filePathString = generateNumberedFileName(directoryPath, extension);
 
-    // Create the file
     std::ofstream outputFile(filePathString);
     
     if (outputFile.is_open()) {
