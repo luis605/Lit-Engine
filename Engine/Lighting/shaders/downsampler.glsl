3f62f35ce4098711b4620a21e4c9f15d4f85941d
luis605 <lusluistomascabetedealmeida@gmail.com>
2024-02-02 19:11:17 +0000

    Improved bloom quality

--- Engine/Lighting/shaders/downsampler.glsl
+++ Engine/Lighting/shaders/downsampler.glsl
@@ -12,7 +12,11 @@ float weight = weightConstant * (1.0 + bloomBrightness) * 10;
 float attenuation = 1.0 - 6.0 * weight;
 vec2 texelSize = vec2(1.0) / textureSize(srcTexture, 0);
 
-uniform int samples = 6;
+uniform int samples = 33; // Increased number of samples
+
+float gaussian(float x, float sigma) {
+    return exp(-(x * x) / (2.0 * sigma * sigma));
+}
 
 void main() {
     vec3 result = texture(srcTexture, fragTexCoord).rgb;
@@ -20,19 +24,19 @@ void main() {
 
     vec2 offsetStep = texelSize * stepSize;
 
-    for (int i = -samples; i <= samples; i += 3) {
-        for (int j = -samples; j <= samples; j += 3) {
+    for (int i = -samples; i <= samples; ++i) {
+        for (int j = -samples; j <= samples; ++j) {
             vec2 offset = vec2(i, j) * offsetStep;
             vec2 texCoordOffset = fragTexCoord + offset;
 
-            vec3 tex1 = texture(srcTexture, texCoordOffset).rgb;
-            result += tex1 * weight;
+            // Clamp texture coordinates to [0,1]
+            texCoordOffset = clamp(texCoordOffset, 0.0, 1.0);
 
-            offset += offsetStep; // Reuse offsetStep
-            texCoordOffset = fragTexCoord + offset;
+            // Use wider Gaussian filter for weighting
+            float gaussWeight = gaussian(length(offset) / (samples * offsetStep.x), 3.0);
 
-            vec3 tex2 = texture(srcTexture, texCoordOffset).rgb;
-            result += tex2 * weight;
+            vec3 tex = texture(srcTexture, texCoordOffset).rgb;
+            result += tex * gaussWeight * weight;
         }
     }
 
