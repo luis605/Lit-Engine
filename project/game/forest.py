88ed2537457b2eae51df8b43a2c6824260383ad2
luis605 <lusluistomascabetedealmeida@gmail.com>
2024-02-23 21:01:10 +0000

    Reviewed memory leak while creating entities using python in edit mode

--- project/game/forest.py
+++ project/game/forest.py
@@ -1,17 +1,18 @@
 import random
 
 
-for i in range(1500):
+for i in range(15):
     pos_x = random.uniform(-250, 250)
     pos_y = 300
     pos_z = random.uniform(-250, 250)
     
     origin = Vector3(pos_x, pos_y, pos_z)
     direction = Vector3(0,-1,0)
-    ray = raycast(origin, direction)
+    ray = Raycast(origin, direction)
     
     if (ray.hit):
         newEntity = Entity(ray.worldPoint, modelPath = "project/game/tree.obj", collider = False)
+
 
 
 
