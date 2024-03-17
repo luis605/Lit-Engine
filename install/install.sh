ec9d608742c488cc3a5fefddb93e845103b1e486
luis605 <lusluistomascabetedealmeida@gmail.com>
2024-03-17 17:15:42 +0000

    Fixed lltb and bullet linker errors

--- install/install.sh
+++ install/install.sh
@@ -7,6 +7,8 @@ sudo apt install libglm-dev
 sudo apt install make
 sudo apt install cmake
 
+sudo apt-get install libtbb-dev
+sudo apt-get install libbullet-dev
 sudo apt-get install build-essential cmake xorg-dev libglu1-mesa-dev
 sudo apt-get install libavutil-dev libavcodec-dev libavformat-dev libswscale-dev
 
