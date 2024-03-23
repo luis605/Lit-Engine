bc228ac6c9071776ea3e2b5f4e3f14c7ea67991b
luis605 <lusluistomascabetedealmeida@gmail.com>
2024-03-23 20:51:28 +0000

    Updated install.sh

--- install/install.sh
+++ install/install.sh
@@ -3,9 +3,7 @@ sudo apt update
 
 sudo apt install python3.11-dev
 sudo apt install python3.11
-sudo apt install ccache
 sudo apt install g++
-sudo apt install libboost-all-dev
 sudo apt install libglm-dev
 sudo apt install make
 sudo apt install cmake
