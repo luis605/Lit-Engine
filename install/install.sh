27653cc6719eb329debdd99a163dc372b3cbc007
luis605 <lusluistomascabetedealmeida@gmail.com>
2024-06-13 22:01:30 +0100

    Updated install.sh to support more linux distributions

--- install/install.sh
+++ install/install.sh
@@ -1,19 +1,43 @@
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo add-apt-repository universe
+#!/bin/bash
 
-sudo apt update
+selectedPackageManager="NONE"
 
-sudo apt install python3.11-dev
-sudo apt install python3.11
-sudo apt install g++
-sudo apt install libglm-dev
-sudo apt install make
-sudo apt install cmake
-sudo apt install libwayland-dev
-sudo apt install libxkbcommon-dev
+# Check for common package managers
+if which apt >/dev/null 2>&1; then
+    echo "Detected package manager: apt"
+    selectedPackageManager="apt"
+elif which yum >/dev/null 2>&1; then
+    echo "Detected package manager: yum"
+    selectedPackageManager="yum"
+elif which dnf >/dev/null 2>&1; then
+    echo "Detected package manager: dnf"
+    selectedPackageManager="dnf"
+elif which pacman >/dev/null 2>&1; then
+    echo "Detected package manager: pacman"
+    selectedPackageManager="pacman"
+else
+    echo "Detected package manager: Unknown"
+fi
 
-sudo apt-get install libtbb-dev
-sudo apt-get install libbullet-dev
-sudo apt-get install build-essential cmake xorg-dev libglu1-mesa-dev
-sudo apt-get install libavutil-dev libavcodec-dev libavformat-dev libswscale-dev
-
+if [ "$selectedPackageManager" = "apt" ]; then
+    sudo add-apt-repository ppa:deadsnakes/ppa universe
+    sudo apt update
+    sudo apt install python3.11-dev python3.11 g++ libglm-dev make cmake libwayland-dev libxkbcommon-dev
+    sudo apt-get install libtbb-dev libbullet-dev build-essential cmake xorg-dev libglu1-mesa-dev libavutil-dev libavcodec-dev libavformat-dev libswscale-dev
+elif [ "$selectedPackageManager" = "yum" ]; then
+    sudo yum install epel-release
+    sudo yum update
+    sudo yum install python3-devel gcc-c++ glm-devel make cmake wayland-devel libxkbcommon-devel
+    sudo yum install tbb-devel bullet-devel @development-tools cmake mesa-libGLU-devel ffmpeg-devel
+elif [ "$selectedPackageManager" = "dnf" ]; then
+    sudo dnf install epel-release
+    sudo dnf update
+    sudo dnf install python3-devel gcc-c++ glm-devel make cmake wayland-devel libxkbcommon-devel
+    sudo dnf install tbb-devel bullet-devel @development-tools cmake mesa-libGLU-devel ffmpeg-devel
+elif [ "$selectedPackageManager" = "pacman" ]; then
+    sudo pacman -Syu
+    sudo pacman -S python python-pip gcc glm make cmake wayland xorg-server xorg-xinit libxkbcommon
+    sudo pacman -S onetbb bullet mesa glu ffmpeg
+else
+    echo "No supported package manager found. Please install the dependencies manually."
+fi
