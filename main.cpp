6ad8979907a077a44e29e74505bb562b0305ce8a
luis605 <lusluistomascabetedealmeida@gmail.com>
2024-06-19 19:05:17 +0100

    Refactored main.cpp

--- main.cpp
+++ main.cpp
@@ -1,39 +1,16 @@
 #include "include_all.h"
 
-int LitEngine()
-{
+int LitEngine() {
     Startup();
-    std::cout << "Lit Engine Started" << std::endl << std::endl << std::endl << std::endl;
+    std::cout << "Lit Engine Started\n\n";
     EngineMainLoop();
     CleanUp();
 
     return 0;
 }
 
-
-int main()
-{
-/*
-    // Create a pipe
-    if (pipe(pipe_fds) == -1) {
-        std::cerr << "Pipe creation failed." << std::endl;
-        return 1;
-    }
-
-
-    pid = fork();
-    if (pid < 0) {
-        std::cerr << "Failed to start Lit Engine - Fork failed." << std::endl;
-    } else if (pid == 0) {
-        setpgid(0, 0);
-        std::cout << "This is the child process." << std::endl;
-        PreviewProject();
-    } else {
-        std::cout << "This is the parent process." << std::endl;
-        LitEngine();
-    }
-*/
-
+int main() {
     LitEngine();
+
     return 0;
 }
