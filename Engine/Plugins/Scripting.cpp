bdbbed7a7b7eb1d7a25df0b11a134133ec73b9fe
luis605 <lusluistomascabetedealmeida@gmail.com>
2024-09-22 19:09:41 +0100

    Skybox plugins' control

--- Engine/Plugins/Scripting.cpp
+++ Engine/Plugins/Scripting.cpp
@@ -1,4 +1,5 @@
 #include "Scripting.h"
+#include "../Lighting/skybox.h"
 
 void initWindow(std::string& windowName, int width, int height, bool resizable) {
     ImGui::SetNextWindowSize(ImVec2(width, height), ImGuiCond_Once);
@@ -13,4 +14,32 @@ void drawText(std::string& text, int x, int y, LitVector3 color) {
     if (x >= 0 && y >= 0) ImGui::SetCursorPos(ImVec2(x, y));
 
     ImGui::TextColored(ImVec4(color.x / 255.0f, color.y / 255.0f, color.z / 255.0f, 1.0f), text.c_str());
+}
+
+bool drawButton(std::string& text, int x, int y, int width, int height, LitVector3 buttonColor, LitVector3 textColor) {
+    if (x >= 0 && y >= 0) ImGui::SetCursorPos(ImVec2(x, y));
+
+    auto applyColor = [](ImGuiCol colorType, const LitVector3& color) {
+        if (color.x >= 0 && color.y >= 0 && color.z >= 0) {
+            ImGui::PushStyleColor(colorType, ImVec4(color.x / 255.0f, color.y / 255.0f, color.z / 255.0f, 1.0f));
+            return true;
+        }
+        return false;
+    };
+
+    bool hasButtonColor = applyColor(ImGuiCol_Button, buttonColor);
+    bool hasTextColor = applyColor(ImGuiCol_Text, textColor);
+
+    bool clicked = (width >= 0 && height >= 0)
+                    ? ImGui::Button(text.c_str(), ImVec2(width, height))
+                    : ImGui::Button(text.c_str());
+
+    if (hasButtonColor) ImGui::PopStyleColor();
+    if (hasTextColor) ImGui::PopStyleColor();
+
+    return clicked;
+}
+
+void setSkybox(const std::string& skyboxPath) {
+    InitSkybox(skyboxPath.c_str());
 }
