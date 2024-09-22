91c8003543af8b5ba2ba06144e536f710fe9d925
luis605 <lusluistomascabetedealmeida@gmail.com>
2024-09-22 15:40:44 +0100

    Initialize plugins

--- Engine/Plugins/Loader.cpp
+++ Engine/Plugins/Loader.cpp
@@ -1,8 +1,52 @@
 #include "Loader.h"
 
+void Plugin::initialize() {
+    try {
+        py::module::import("sys").attr("path").attr("append")(m_path.c_str());
+
+        m_module = py::module::import("main");
+
+        if (m_module.attr("initPlugin")) {
+            m_module.attr("initPlugin")();
+        } else {
+            TraceLog(LOG_WARNING, ("Initialization function not found in plugin: " + m_name).c_str());
+        }
+    } catch (const py::error_already_set& e) {
+        TraceLog(LOG_ERROR, ("Failed to initialize plugin '" + m_name + "': " + std::string(e.what())).c_str());
+    }
+}
+
+
+void Plugin::unload() {
+    try {
+        py::scoped_interpreter guard{};
+
+        if (m_module.attr("unloadPlugin")) {
+            m_module.attr("unloadPlugin")();
+        } else {
+            TraceLog(LOG_WARNING, ("Unload function not found in plugin: " + m_name).c_str());
+        }
+    } catch (const py::error_already_set& e) {
+        TraceLog(LOG_ERROR, ("Failed to unload plugin '" + m_name + "': " + std::string(e.what())).c_str());
+    }
+}
+
 void Plugins::load(const std::string& name, const std::string& path) {
     m_plugins.insert({name, std::make_unique<Plugin>(name, path)});
     TraceLog(LOG_INFO, ("Loaded plugin: " + name).c_str());
+}
+
+void Plugins::initializeAllPlugins() {
+    for (const auto& [name, plugin] : m_plugins) {
+        plugin->initialize();
+    }
+}
+
+void Plugins::unloadAll() {
+    for (const auto& [name, plugin] : m_plugins) {
+        plugin->unload();
+    }
+    m_plugins.clear();
 }
 
 void loadAllPlugins() {
@@ -41,6 +85,8 @@ void loadAllPlugins() {
             TraceLog(LOG_ERROR, message.c_str());
         }
     }
+
+    pluginManager.initializeAllPlugins();
 }
 
 void unloadAllPlugins() {
